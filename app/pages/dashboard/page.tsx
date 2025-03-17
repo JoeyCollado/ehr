f10f6739298548f9fe812f7f91b72ba2ea184589
Joey Collado <akosijoeyaibertcollado@gmail.com>
2025-03-17 19:59:22 +0800

    authentication done

---
+++ app/pages/dashboard/page.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import React from "react";
+
+
+const Page = () => {
+
+
+
+  return (
+    <>
+      <div className="md:text-7xl text-5xl text-center mt-20 h-screen">
+        EHR
+      </div>
+    </>
+  );
+};
+
+export default Page;
