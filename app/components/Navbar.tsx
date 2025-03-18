02124c62230530a10909b68795f1f906cb6debb0
Joey Collado <akosijoeyaibertcollado@gmail.com>
2025-03-18 09:19:19 +0800

    navbar improvement

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,11 +1,37 @@
-import React from 'react'
+import Link from 'next/link';
+import { FaHome, FaHeartbeat, FaFileMedical, FaVials, FaCapsules, FaNotesMedical } from 'react-icons/fa';
 
 const Navbar = () => {
   return (
-    <div className='bg-gray-500 w-24 h-full absolute'>
-      Navbar
+    <div className="bg-gray-800 text-white w-60 h-screen fixed p-4">
+      <ul className="space-y-10 mt-[10%]">
+        <li className="flex items-center gap-2 hover:bg-gray-700 p-2 rounded-md">
+          <FaHome />
+          <Link href="/">Home</Link>
+        </li>
+        <li className="flex items-center gap-2 hover:bg-gray-700 p-2 rounded-md">
+          <FaFileMedical />
+          <Link href="/">Medical History</Link>
+        </li>
+        <li className="flex items-center gap-2 hover:bg-gray-700 p-2 rounded-md">
+          <FaHeartbeat />
+          <Link href="/">Health Assessment</Link>
+        </li>
+        <li className="flex items-center gap-2 hover:bg-gray-700 p-2 rounded-md">
+          <FaVials />
+          <Link href="/">Vital Sheet</Link>
+        </li>
+        <li className="flex items-center gap-2 hover:bg-gray-700 p-2 rounded-md">
+          <FaCapsules />
+          <Link href="/">Laboratory Results</Link>
+        </li>
+        <li className="flex items-center gap-2 hover:bg-gray-700 p-2 rounded-md">
+          <FaNotesMedical />
+          <Link href="/">Medical Administration Record</Link>
+        </li>
+      </ul>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
