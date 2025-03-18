33f7695e312003d84e0cf53f09a77d89f39254e5
Joey Collado <akosijoeyaibertcollado@gmail.com>
2025-03-18 09:22:26 +0800

    navbar improvement2

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -4,28 +4,29 @@ import { FaHome, FaHeartbeat, FaFileMedical, FaVials, FaCapsules, FaNotesMedical
 const Navbar = () => {
   return (
     <div className="bg-gray-800 text-white w-60 h-screen fixed p-4">
-      <ul className="space-y-10 mt-[10%]">
+      <span className='font-bold text-5xl ml-2'>Logo</span>
+      <ul className="space-y-10 mt-[30%]">
         <li className="flex items-center gap-2 hover:bg-gray-700 p-2 rounded-md">
           <FaHome />
           <Link href="/">Home</Link>
         </li>
-        <li className="flex items-center gap-2 hover:bg-gray-700 p-2 rounded-md">
+        <li className="flex items-center gap-3 hover:bg-gray-700 p-2 rounded-md">
           <FaFileMedical />
           <Link href="/">Medical History</Link>
         </li>
-        <li className="flex items-center gap-2 hover:bg-gray-700 p-2 rounded-md">
+        <li className="flex items-center gap-3 hover:bg-gray-700 p-2 rounded-md">
           <FaHeartbeat />
           <Link href="/">Health Assessment</Link>
         </li>
-        <li className="flex items-center gap-2 hover:bg-gray-700 p-2 rounded-md">
+        <li className="flex items-center gap-3 hover:bg-gray-700 p-2 rounded-md">
           <FaVials />
           <Link href="/">Vital Sheet</Link>
         </li>
-        <li className="flex items-center gap-2 hover:bg-gray-700 p-2 rounded-md">
+        <li className="flex items-center gap-3 hover:bg-gray-700 p-2 rounded-md">
           <FaCapsules />
           <Link href="/">Laboratory Results</Link>
         </li>
-        <li className="flex items-center gap-2 hover:bg-gray-700 p-2 rounded-md">
+        <li className="flex items-center gap-3 hover:bg-gray-700 p-2 rounded-md">
           <FaNotesMedical />
           <Link href="/">Medical Administration Record</Link>
         </li>
