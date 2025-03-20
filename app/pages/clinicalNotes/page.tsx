54785c60380bc9a0d2aa7d9156fee55bb6d99b5c
Joey Collado <akosijoeyaibertcollado@gmail.com>
2025-03-20 21:42:34 +0800

    adjustments to bg color

--- app/pages/clinicalNotes/page.tsx
+++ app/pages/clinicalNotes/page.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const Page = () => {
   return (
-    <div className="md:text-7xl h-screen bg-gradient-to-b from-[#FEC5BB] via-[#FAE1DD] text-[#3A2B22] h-scren text-5xl text-center flex-grow  flex items-center justify-center">
+    <div className="md:text-7xl h-screen bg-white text-[#3A2B22] h-scren text-5xl text-center flex-grow  flex items-center justify-center">
       Clinical Notes
     </div>
   );
--- app/pages/home/page.tsx
+++ app/pages/home/page.tsx
@@ -7,7 +7,7 @@ const Page = () => {
 
   return (
     <>
-    <div className="h-screen font-sans bg-white text-[#3A2B22] flex flex-col items-center text-center p-6">
+    <div className="h-screen font-sans bg-white  text-[#3A2B22] flex flex-col items-center text-center p-6">
       {/* Welcome Message */}
       <h1 className="text-6xl md:text-7xl font-bold mt-[15%]">Welcome Back!</h1>
       <p className="text-xl mt-4">Manage your health records with ease</p>
