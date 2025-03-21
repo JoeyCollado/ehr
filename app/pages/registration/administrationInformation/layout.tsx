c90512ee1774f794bd09d4a898976bd859901d58
Joey Collado <akosijoeyaibertcollado@gmail.com>
2025-03-21 17:53:39 +0800

    improve navigation header

--- app/pages/registration/administrationInformation/layout.tsx
+++ app/pages/registration/administrationInformation/layout.tsx
@@ -7,13 +7,13 @@ export default function Layout({ children }: { children: React.ReactNode }) {
         <div className="h-screen font-sans bg-white  text-[#3A2B22] flex flex-col items-center text-center ">
       <h1 className="text-6xl md:text-7xl font-bold mt-[5%]">Administration Information</h1>
 
-      <div className="bottom-0 h-[10vh] bg-slate-500 w-full">
+      <div className="bottom-0 h-[10vh] bg-gradient-to-b from-[#E67E6B] via-[#D65A4A] to-[#A8443B] text-white w-full">
         <ul className="flex gap-10 text-center ml-[25%]">
             <li><Link href="/pages/registration/administrationInformation/vitalSigns">Vital Signs Sheet</Link></li>
-            <li><Link href="/pages/registration/administrationInformation/chiefComplaint">Vital Signs Sheet</Link></li>
-            <li><Link href="/pages/registration/administrationInformation/medicalHistory">Vital Signs Sheet</Link></li>
-            <li><Link href="/pages/registration/administrationInformation/immunization">Vital Signs Sheet</Link></li>
-            <li><Link href="/pages/registration/administrationInformation/healthAssessment">Vital Signs Sheet</Link></li>
+            <li><Link href="/pages/registration/administrationInformation/chiefComplaint">Chief Complaint</Link></li>
+            <li><Link href="/pages/registration/administrationInformation/medicalHistory">HealthAssessment</Link></li>
+            <li><Link href="/pages/registration/administrationInformation/immunization">Immunization</Link></li>
+            <li><Link href="/pages/registration/administrationInformation/healthAssessment">Medical History</Link></li>
         </ul>
       </div>  
     </div>
