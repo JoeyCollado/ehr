c2b79cd4ea622d6b268d3294eb6315dff2ba14db
Joey Collado <akosijoeyaibertcollado@gmail.com>
2025-03-27 15:13:24 +0800

    table added

--- app/pages/clinicalNotes/mar/page.tsx
+++ app/pages/clinicalNotes/mar/page.tsx
@@ -51,9 +51,40 @@ const Page = () => {
   
   return (
     <>
-    <button onClick={handleEdit} className='text-center text-1xl cursor-pointer rounded-md px-3 text-white bg-blue-500 justify-center '></button>
+     <button
+        onClick={handleEdit}
+        className="text-center text-1xl cursor-pointer rounded-md px-3 text-white bg-blue-500 justify-center ml-[50%] mt-[5%]"
+      >
+        {isEditing ? "Save" : "Edit"}
+      </button>
     <div className='h-screen flex items-center justify-center bg-[#ffffff] text-[#3A2B22] mb-[20%] '>
-      <div className='bg-red-400 h-[100vh] w-[80%] mt-[20%]'></div>
+      <div className='bg-red-400 h-[100vh] w-[80%] mt-[20%]'>
+        <table>
+            <thead>
+                <tr>
+                    <th className='border'>Name:</th>
+                    <th className='border'>Start Date:</th>
+                    <th className='border'>End Date:</th>
+                    <th className='border'>Date of Birth:</th>
+                    <th className='border'>Doctor:</th>
+                    <th className='border'>Known Allergies:</th>
+                    <th className='border'>Address:</th>
+                </tr>
+            </thead>
+
+            <tbody>
+                <tr className='border'>
+                    <td className='border'>
+                        {isEditing ? (
+                            <input type='text' value={details.name} onChange={(e) => handleChange(e, "name")} className='w-full border p-1'/> ) : (
+                                <strong>{details.name}</strong>
+                        )}
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+
+      </div>
       
     </div>
     </>
