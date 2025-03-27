1070057f6de3e71be543c648c19506342c5b7633
Joey Collado <akosijoeyaibertcollado@gmail.com>
2025-03-27 15:02:51 +0800

    edit button

--- app/pages/clinicalNotes/mar/page.tsx
+++ app/pages/clinicalNotes/mar/page.tsx
@@ -2,7 +2,7 @@
 
 import React, {useState, useEffect} from 'react'
 
-const page = () => {
+const Page = () => {
 
   //
   const [isEditing, setIsEditing] = useState(false);
@@ -33,11 +33,12 @@ const page = () => {
 
 
   //toggle edit
-  const handleEdit(() => {
+  const handleEdit = () => {
     if(isEditing){
       localStorage.setItem('details', JSON.stringify(details))
     }
-  })
+    setIsEditing(!isEditing);
+  };
 
   //handle table changes
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>, key: string) => {
@@ -45,15 +46,18 @@ const page = () => {
   };
 
   //
-  if(hasMounted) return null;
+  //if(hasMounted) return null;
 
   
   return (
+    <>
+    <button onClick={handleEdit} className='text-center text-1xl cursor-pointer rounded-md px-3 text-white bg-blue-500 justify-center '></button>
     <div className='h-screen flex items-center justify-center bg-[#ffffff] text-[#3A2B22] mb-[20%] '>
-      <div className='bg-red-300 h-[100vh] w-[80%] mt-[20%]'></div>
+      <div className='bg-red-400 h-[100vh] w-[80%] mt-[20%]'></div>
       
     </div>
+    </>
   )
 }
 
-export default page
+export default Page;
