1a16e2b0737fe44d7626ef920f7387d70adf32b6
Joey Collado <akosijoeyaibertcollado@gmail.com>
2025-03-27 14:47:00 +0800

    mar localstorage

--- app/pages/clinicalNotes/mar/page.tsx
+++ app/pages/clinicalNotes/mar/page.tsx
@@ -3,9 +3,12 @@
 import React, {useState, useEffect} from 'react'
 
 const page = () => {
+
+  //
   const [isEditing, setIsEditing] = useState(false);
   const [hasMounted, setHasMounted] = useState(false);
 
+  //
   const defaultDetails = {
     name: "",
     birthDate: "",
@@ -16,9 +19,34 @@ const page = () => {
     Address: "",
   }
 
-  const defaultDatas = [
-    
-  ] 
+  //
+  const [details, setDetails] = useState(defaultDetails);
+
+
+  //hydration
+  useEffect(() => {
+    setHasMounted(true);
+    const savedDetails = localStorage.getItem("details");
+
+    if(savedDetails) setDetails(JSON.parse(savedDetails));
+  }, []);
+
+
+  //toggle edit
+  const handleEdit(() => {
+    if(isEditing){
+      localStorage.setItem('details', JSON.stringify(details))
+    }
+  })
+
+  //handle table changes
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>, key: string) => {
+    setDetails({...details, [key]: e.target.value});
+  };
+
+  //
+  if(hasMounted) return null;
+
   
   return (
     <div className='h-screen flex items-center justify-center bg-[#ffffff] text-[#3A2B22] mb-[20%] '>
