c8dc0ac9d8e99b9f48ca89965437ab6480053c4f
Joey Collado <akosijoeyaibertcollado@gmail.com>
2025-03-28 10:22:13 +0800

    key

--- middleware.ts
+++ middleware.ts
@@ -14,3 +14,6 @@ export const config = {
     '/(api|trpc)(.*)',
   ],
 };
+
+console.log("Clerk Publishable Key:", process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY);
+console.log("Clerk Secret Key:", process.env.CLERK_SECRET_KEY);
