e45bedb8c33c73546c77d5d6747cb699e7a3293d
Joey Collado <akosijoeyaibertcollado@gmail.com>
2025-04-01 11:54:54 +0800

    savepoint

--- app/pages/clinicalNotes/consults/page.tsx
+++ app/pages/clinicalNotes/consults/page.tsx
@@ -61,7 +61,7 @@ const Page = () => {
 
   return (
     <>
-     <div className="flex justify-center items-center gap-4 mt-5 mb-2 mt-[5%]">
+     <div className="flex justify-center items-center gap-4 mb-5 mt-[5%]">
   <button
     onClick={handleEdit}
     className="text-1xl cursor-pointer rounded-md px-3 text-white bg-[#007bff] py-1 hover:bg-blue-700 hover:scale-105 hover:shadow-lg transition-transform"
@@ -79,7 +79,7 @@ const Page = () => {
       <div className="min-h-screen bg-[#faf6f6] text-[#3A2B22] flex flex-col items-center pl-[10%] pr-[10%] shadow-lg ">
         <div className={`w-full ${entries.length > 10 ? 'max-h-[500px] overflow-y-auto' : ''}`}>
             
-          <table className="w-full border-collapse border ">
+          <table className="w-full border-collapse border bg-white">
             <thead>
               <tr className="border">
                 <th className="border px-2">Consult Report ID</th>
@@ -111,7 +111,7 @@ const Page = () => {
                     </td>
                   ))}
                   {isEditing && (
-                    <td className="border px-2 text-center">
+                    <td className="border px-2 text-center py-2">
                       <button
                         onClick={() => deleteEntry(index)}
                         className="text-white bg-red-600 px-2 py-1 rounded-md hover:bg-red-700"
