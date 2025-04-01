3acf1700a4474dfaa1b842d3e690280bf93ef244
Joey Collado <akosijoeyaibertcollado@gmail.com>
2025-04-01 11:15:16 +0800

    consults

---
+++ app/pages/clinicalNotes/consults/page.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <>
+      <div className="min-h-screen bg-[#faf6f6] text-[#3A2B22] flex flex-col items-center p-6 shadow-lg ">
+        <table>
+          <thead>
+            <tr className="border">
+              <th className="border">Consult Report ID</th>
+              <th className="border">Consult Report ID</th>
+              <th className="border">Consult Report ID</th>
+              <th className="border">Consult Report ID</th>
+              <th className="border">Consult Report ID</th>
+              <th className="border">Consult Report ID</th>
+              <th className="border">Consult Report ID</th>
+            </tr>
+          </thead>
+        </table>
+      </div>
+    </>
+  );
+};
+
+export default page;
