5d97b1a849a733d7264408baa3e6dfa2c4c5e4c4
Joey Collado <akosijoeyaibertcollado@gmail.com>
2025-04-06 16:47:56 +0800

    savepoint

--- app/pages/cdss/diagnosis/page.tsx
+++ app/pages/cdss/diagnosis/page.tsx
@@ -4,7 +4,7 @@ const page = () => {
   return (
     <div className="min-h-screen bg-[#faf6f6] flex items-center justify-center pb-4 mb-[5%]">
       <div className="w-full max-w-5xl bg-white text-black shadow-lg rounded-lg p-6">
-        
+        <h1 className="font-bold text-3xl text-center mb-5">Diagnosis</h1>
       </div>
     </div>
   );
